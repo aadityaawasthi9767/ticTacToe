11abec2fd5603215cd9ea85a06e6af02cfbf26c1
aadityaawasthi9767 <aadityaawasthi9767@gmail.com>
2020-07-15 22:00:02 +0530

    [Aaditya]Added: player and reset board

---
+++ tictTacToe.sh
@@ -0,0 +1,23 @@
+#! /bin/bash 
+
+#variables
+START=0;
+ROW=2;
+COLUMN=0;
+
+#ARRAY
+declare -a playBoard;
+
+function reset(){
+player=$((START));
+
+for((rowIndex=0;rowIndex<=$ROW;rowIndex++))
+do
+	for((columnIndex=0;columnIndex<=$COLUMN;columnIndex++))
+	do
+		playBoard[$rowIndex,$columnIndex]=0;
+	done
+done
+echo "Board Resetted";
+}
+reset
